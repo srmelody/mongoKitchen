88d94d816833818f311d31dfb06a02bc97e7e2b2
SR Melody <srmelody@gmail.com>
2012-03-04 08:48:29 -0500

    Removed dogs, added layout links

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all( :sort => :title.asc )
 
     respond_to do |format|
       format.html # index.html.erb
